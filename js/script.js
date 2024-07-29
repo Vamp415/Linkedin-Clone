852b003415b345b055f14d3fc01daeef75655141
Vamp415 <vchs415@gmail.com>
2024-07-29 10:35:32 +0530

    add profile menu wrap js script and fix html and css code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// <==================== profile-menu-wrap script start ====================>
+let profileMenu = document.getElementById("profileMenu");
+
+function toggleMenu() {
+  profileMenu.classList.toggle("open-menu");
+}
+// <==================== profile-menu-wrap script End ====================>
